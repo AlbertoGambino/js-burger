9ac30a12a474b87b2f12866485dbc362a1ebf888
Alberto Gambino <gambafb@libero.it>
2021-03-11 20:06:27 +0100

    Javascript + CSS(wip)

--- script.js
+++ script.js
@@ -2,36 +2,55 @@
 var priceBtn = document.getElementById('calculate');
 priceBtn = addEventListener('click', function(){
 
-var nomePanino = document.getElementById('burger-name').value;
+  var nomePanino = document.getElementById('burger-name').value;
 
-if(nomePanino < 1 ){
-  alert('Per continuare dai un nome al tuo panino')
-} else{
+  if(nomePanino === '' ){
+    alert('Per continuare dai un nome al tuo panino')
+    return
 
-  var ingredients = document.getElementsByClassName('ingred');
+  }
+    var ingredients = document.getElementsByClassName('ingred');
 
-  var prezzoMinimo = 10;
-  for(var i = 0; i < ingredients.length; i++ ){
+    var prezzoMinimo = 10;
 
-    var ingredient = ingredients[i];
-    var ingFlag = ingredient.checked;
-    var costoAggiuntivo = parseInt(ingredient.dataset.price);
+    for(var i = 0; i < ingredients.length; i++ ){
 
-    if(ingFlag){
-      prezzoMinimo += costoAggiuntivo;
+      var ingredient = ingredients[i];
+      var isChecked = ingredient.checked;
+
+
+      if(isChecked){
+
+        prezzoMinimo += parseInt(ingredient.dataset.price);
+      }
     }
+    var coupons = ['fortuna','luck','gratis'];
+    var couponInserito = document.getElementById('burger-coupon').value;
+
+    if(coupons.includes(couponInserito)){
+      prezzoMinimo -= prezzoMinimo / 5;
+    }
+
+    var spazioPrice = document.getElementById('price');
+    spazioPrice.innerHTML = prezzoMinimo;
+
+  });
+
+  var liste = document.getElementsByTagName('li');
+  for (var i = 0; i < liste.length; i++) {
+
+    var li = liste[i];
+    li.addEventListener('click', function(){
+
+      console.log(li);
+
+      var wholeLi = this;
+      var childsLi = this.children;
+      var child2Li = childsLi[1];
+
+      child2Li.checked = !child2Li.checked;
+
+    })
+
+
   }
-  var coupons = ['fortuna','luck','gratis'];
-  var couponInserito = document.getElementById('burger-coupon').value;
-
-  if(coupons.includes(couponInserito)){
-    prezzoMinimo -= prezzoMinimo / 5;
-  }
-
-  var spazioPrice = document.getElementById('price');
-  spazioPrice.innerHTML = prezzoMinimo;
-
-
-}
-
-})
